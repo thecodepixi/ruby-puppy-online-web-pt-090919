4be562a39a3b6d8c6b7aead9a8111d6397e06e10
Emily Harber <emily.a.harber@gmail.com>
2019-09-26 17:08:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,13 @@ class Dog
   attr_accessor :name 
   @@all = []
   
-  def save
-    @@all << self
-  end 
-  
   def initialize(name)
     @name = name 
     self.save 
+  end 
+  
+  def save
+    @@all << self
   end 
  
   def self.all 
